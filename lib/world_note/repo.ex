22771ee8e288e3fa08117f45df3d36f059f2a2e0
Krzysztof Wende <iraasta@gmail.com>
2015-06-05 16:35:22 +0200

    api

--- lib/world_note/repo.ex
+++ lib/world_note/repo.ex
@@ -1,3 +1,12 @@
 defmodule WorldNote.Repo do
   use Ecto.Repo, otp_app: :world_note
+
+  schema "chats" do
+    add :fbid, :integer
+    add :latitude, :float
+    add :longitude, :float
+    add :content, :string
+
+    timestamps
+  end
 end
