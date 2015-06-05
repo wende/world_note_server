bf728665cc27f26e0e0e85910d5b3062c4636362
Krzysztof Wende <iraasta@gmail.com>
2015-06-05 15:29:05 +0200

    api

--- web/controllers/chats_controller.ex
+++ web/controllers/chats_controller.ex
@@ -1,10 +1,41 @@
 defmodule WorldNote.ChatsController do
   use WorldNote.Web, :controller
-
+  import Ecto.Query
   alias WorldNote.Chats
 
+  plug :allow_origin
   plug :scrub_params, "chats" when action in [:create, :update]
   plug :action
+
+  defp allow_origin(conn, _opts) do
+    headers = get_req_header(conn, "access-control-request-headers") |> Enum.join(", ")
+
+    conn
+    |> put_resp_header("access-control-allow-origin", "*")
+    |> put_resp_header("access-control-allow-headers", headers)
+    |> put_resp_header("access-control-allow-methods", "get, post, options")
+    |> put_resp_header("access-control-max-age", "3600")
+  end
+
+  def parse(string) do
+    {a, ""} = Float.parse(string)
+    a
+  end
+
+  def index(conn, %{"lat" => lat, "lng" => lng}) do
+    # +-0.1 bounds of both
+    latH = parse(lat) + 0.1
+    latL = parse(lat) - 0.1
+    lngH = parse(lng) + 0.1
+    lngL = parse(lng) - 0.1
+
+    query = from w in Chats,
+            where: w.latitude   < ^latH and w.latitude  > ^latL
+              and  w.longitude  < ^lngH and w.longitude > ^lngL,
+            select: w
+    chats = Repo.all(query)
+    render(conn, "index.json", chats: chats)
+  end
 
   def index(conn, _params) do
     chats = Repo.all(Chats)
