d965b7c16174d36c922c3547c35f189b32163f3c
Krzysztof Wende <iraasta@gmail.com>
2015-06-05 16:45:50 +0200

    migrate

---
+++ priv/repo/migrations/migration.exs
@@ -0,0 +1,14 @@
+defmodule HelloPhoenix.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def change do
+    create table(:chats) do
+      add :fbid, :integer
+      add :latitude, :float
+      add :longitude, :float
+      add :content, :string
+
+      timestamps
+    end
+  end
+end
